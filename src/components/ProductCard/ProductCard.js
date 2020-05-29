fe1bb3fb853693d36643404d4b820ff9893af363
Nisan Ercan Tarhan <nisan_tarhan@hotmail.com>
2020-05-30 01:30:10 +0300

    The layout of ProductCard is completed

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import { StyledProductCard } from './styles';
+import { StyledProductCard, StyledImage, StyledText, StyledPriceTag, StyledCargoDescription } from './styles';
 
 const ProductCard = () => {
     return (
         <StyledProductCard>
-            ProductCard
+            <StyledImage src={"https://dummyimage.com/159x241"} alt="product" />
+            <StyledText>Apple iPhone Apple iPhone  Apple iPhone 11 Pro Max 256 GB Altın Cep Telefonu (Apple Türkiye Garantili)</StyledText>
+            <StyledPriceTag>3.4999,00 TL</StyledPriceTag>
+            <StyledCargoDescription>Ücretsiz Kargo</StyledCargoDescription>
         </StyledProductCard>
     )
 }
--- src/components/ProductCard/styles.js
+++ src/components/ProductCard/styles.js
@@ -6,6 +6,7 @@ export const StyledProductCard = styled.div`
     width: 288px;
     height: 400px;
     margin: auto;
+    padding: 10px;
     background: orange;
     /* box-shadow: 6px 9px 34px 0px rgba(0,0,0,0.15); */
     cursor: pointer;
@@ -13,4 +14,51 @@ export const StyledProductCard = styled.div`
     border-radius: 5px;
     transition: transform 0.25s ease-out;
     position: relative;
+`
+
+export const StyledImage = styled.img`
+    width: 100%;
+    height: 24.4rem;
+    border-radius: 3px;
+    margin-bottom: 0.75rem;
+`
+
+export const StyledText = styled.p`
+    background: white;
+    font-size: 13px;
+    font-weight: 600;
+    padding-left: 10px;
+    padding-bottom: 10px;
+    margin-bottom: 10px;
+    line-height: 16px;
+    height: 40px;
+    /* width: 200px; */
+    /* box-orient: vertical;   */
+    line-clamp: 2;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    /* color: rgb(51, 51, 58); */
+`
+
+export const StyledPriceTag = styled.p`
+    background: #CCCCCC;
+    width: 98%;
+    border-radius: 6px;
+    font-size: 20px;
+    font-weight: 700;
+    color: #333;
+    padding-left: 7px;
+    padding-bottom: 5px;
+    margin: 0;
+    line-height: 1.6rem;
+`
+
+export const StyledCargoDescription = styled.p`
+    background: white;
+    font-size: 12px;
+    font-weight: 600;
+    padding-left: 10px;
+    padding-bottom: 10px;
+    color: #5ba71b;
 `
